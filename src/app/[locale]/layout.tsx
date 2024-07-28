9d43be35eecba549b8356b9cf0549fb87c725d50
foullanifatimazohra <foullanifatoum@gmail.com>
2024-07-28 15:56:18 +0100

    refactoring

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
 
-import { Inter } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 
 import "../globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +20,8 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
-      <body className={inter.className}>
+    <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+      <body>
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
