44b4a734c502e40ad1fa0350440f35ae74c64465
foullanifatimazohra <foullanifatoum@gmail.com>
2024-07-30 14:20:38 +0100

    feat(home): implement hero section

--- src/views/home/Hero.tsx
+++ src/views/home/Hero.tsx
@@ -4,13 +4,19 @@ import Image from "next/image";
 import Button from "@/components/ui/Button";
 
 import Mask from "../../../public/assets/images/home/mask.svg";
+
+const companiesLogos = [
+  "bank-muskat.svg",
+  "ministry-of-finance.svg",
+  "world-health.svg",
+];
 async function Hero() {
   const t = await getTranslations();
 
   return (
-    <section className="container py-8 flex flex-col gap-6 items-center px-2 md:mx-auto h-screen">
+    <section className="container py-8 flex flex-col gap-6 items-center px-2 md:mx-auto">
       <h1
-        className="text-h1 text-center text-primary-gray-900 max-w-[25ch] font-bold"
+        className="text-h1 text-center leading-[66px] text-primary-gray-900 max-w-[25ch] font-bold"
         dangerouslySetInnerHTML={{
           __html: t.markup("home.hero.title", {
             highlight: (chunks: string) =>
@@ -18,17 +24,19 @@ async function Hero() {
           }),
         }}
       />
-      <p className="text-body text-primary-gray-900 text-center max-w-[60ch]">
+      <p className="text-body text-primary-gray-900 font-normal leading-6 text-center max-w-[60ch]">
         {t("home.hero.paragraph")}
       </p>
       <Button size="xlarge">{t("home.hero.cta")}</Button>
       {maskVideo}
+      {renderInfo(t)}
     </section>
   );
 }
 
+// ----------------------------------------------------------------
 const maskVideo = (
-  <div className="relative w-full h-full">
+  <div className="relative w-full md:min-h-[300px] h-[200px] mt-10">
     <video
       className="absolute top-0 left-0 w-full h-full object-fill"
       style={{
@@ -47,4 +55,31 @@ const maskVideo = (
   </div>
 );
 
+// ----------------------------------------------------------------
+
+const renderInfo = (t: any) => (
+  <div className="flex w-full max-sm:flex-col gap-10 items-center md:justify-between mt-10">
+    <div className="flex gap-4 max-sm:flex-col">
+      {t.raw("home.hero.info").map((card: any, index: number) => (
+        <div key={index}>
+          <h3 className="font-bold text-body text-primary-gray-900">
+            {card.count}
+          </h3>
+          <p className="text-body font-normal">{card.text}</p>
+        </div>
+      ))}
+    </div>
+    <div className="flex gap-10 max-sm:flex-col">
+      {companiesLogos.map((companyLogo: any, index: number) => (
+        <Image
+          key={index}
+          src={`/assets/images/companies/${companyLogo}`}
+          alt={companyLogo}
+          width={110}
+          height={28}
+        />
+      ))}
+    </div>
+  </div>
+);
 export default Hero;
