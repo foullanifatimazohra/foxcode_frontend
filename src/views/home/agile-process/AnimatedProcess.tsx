061435e7153150cca1b129e6f737d5493d73f068
foullanifatimazohra <foullanifatoum@gmail.com>
2024-08-09 12:07:34 +0100

    feat(animatedProcess): implement the appearance animation

--- src/views/home/agile-process/AnimatedProcess.tsx
+++ src/views/home/agile-process/AnimatedProcess.tsx
@@ -1,77 +1,60 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 import { useLocale, useTranslations } from "next-intl";
-import { motion, useAnimation, useInView } from "framer-motion";
-
-const cardVariants = {
-  hidden: { opacity: 0, x: -100 },
-  visible: (i: number) => ({
-    opacity: 1,
-    x: 0,
-    transition: { delay: i * 0.2 },
-  }),
-};
+import { motion, useScroll, useTransform } from "framer-motion";
 
 function AnimatedProcess() {
-  const controls = useAnimation();
-
   const locale: string = useLocale();
 
   const t = useTranslations();
 
-  const ref = useRef(null);
+  const animatedSectionRef = useRef(null);
 
-  const isInView = useInView(ref);
+  const { scrollYProgress } = useScroll({ target: animatedSectionRef });
 
-  useEffect(() => {
-    if (isInView) {
-      controls.start((i) => ({
-        opacity: 1,
-        x: 0,
-        transition: { delay: i * 0.2 },
-      }));
-    } else {
-      controls.start("hidden");
-    }
-  }, [isInView, controls]);
+  let sequentialNumber = 0;
 
   return (
     <section
-      ref={ref}
+      ref={animatedSectionRef}
       className="mt-[55px] border-t flex justify-between gap-4 max-sm:divide-y rtl:divide-x-reverse md:divide-x divide-primary-gray-200 flex-wrap border-primary-gray-200"
     >
       {t.raw("home.agileProcess.process").map((process: any, index: number) => (
-        <motion.div
-          key={index}
-          initial="hidden"
-          animate={controls}
-          custom={index}
-          className="rtl:pr-3 pl-3"
-          variants={cardVariants}
-        >
+        <motion.div key={index} className="rtl:pr-3 pl-3 flex-1">
           <p className="my-8 text-[20px] font-bold">{process.title}</p>
-          {process.steps.map((text: string, stepIndex: number) => (
-            <motion.div
-              key={stepIndex}
-              className={`flex w-fit  rtl:mr-[${stepIndex * 20}px] ml-[${stepIndex * 20}px] gap-4 px-2 py-1 rounded-[72px] border border-primary-gray-100 items-center`}
-              initial="hidden"
-              animate={controls}
-              custom={stepIndex}
-              style={{
-                [locale === "ar" ? "marginRight" : "marginLeft"]:
-                  `${stepIndex * 20}px`,
-                marginTop: "16px",
-              }}
-              variants={cardVariants}
-            >
-              <p className="h-[32px] w-[32px] flex items-center justify-center rounded-full bg-primary-gray-200">
-                {index * process.steps.length + stepIndex + 1}{" "}
-                {/* Sequential Numbering */}
-              </p>
-              <p className="font-medium">{text}</p>
-            </motion.div>
-          ))}
+          {process.steps.map((text: string, stepIndex: number) => {
+            // Incremental X Transform for each step
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            const xTransform = useTransform(
+              scrollYProgress,
+              [1, 0],
+              locale === "ar"
+                ? [(stepIndex + 1) * -1, stepIndex * -30] // Reversed for RTL
+                : [stepIndex * 0, (stepIndex + 1) * 30] // Normal for LTR
+            );
+
+            sequentialNumber = sequentialNumber + 1;
+
+            return (
+              <motion.div
+                key={stepIndex}
+                initial={{ x: 0 }}
+                className={`flex w-fit gap-4 px-2 py-1 rounded-[72px] border border-primary-gray-100 items-center`}
+                style={{
+                  marginTop: "16px",
+                  [locale === "ar" ? "marginRight" : "marginLeft"]:
+                    `${stepIndex * 20}px`,
+                  x: xTransform,
+                }}
+              >
+                <p className="h-[32px] w-[32px] flex items-center justify-center rounded-full bg-primary-gray-200">
+                  {sequentialNumber}
+                </p>
+                <p className="font-medium">{text}</p>
+              </motion.div>
+            );
+          })}
         </motion.div>
       ))}
     </section>
