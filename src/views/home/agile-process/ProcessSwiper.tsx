b8424c37250a4359495849a024188a9e144eff13
foullanifatimazohra <foullanifatoum@gmail.com>
2024-08-10 01:41:49 +0100

    feat(home): improve the process swiper implementation

--- src/views/home/agile-process/ProcessSwiper.tsx
+++ src/views/home/agile-process/ProcessSwiper.tsx
@@ -12,24 +12,25 @@ function ProcessSlider() {
   const t = useTranslations();
   return (
     <Swiper
-      spaceBetween={10}
+      spaceBetween={60}
       slidesPerView="auto"
       scrollbar={{ draggable: true }}
-      className="my-[100px]"
+      className="my-[100px] min-h-screen overflow-visible lg:!pt-[100px]"
     >
       {t.raw("home.agileProcess.cards").map((card: any, index: number) => (
-        <SwiperSlide key={index} className="h-full">
-          <div className="slider-gradient h-full relative items-start lg:min-w-[1200px] text-start rounded-lg p-5 ">
-            <div className="max-w-[80ch] h-full z-10 md:p-[82px] flex flex-col gap-[42px]">
+        <SwiperSlide key={index} className="!h-auto md:!w-fit">
+          <div className="h-fit w-full items-start lg:min-w-[1200px] text-start rounded-lg p-5 overflow-visible">
+            <div className="max-w-[70ch] h-full z-10 md:p-[82px] flex flex-col gap-[42px]">
               <div
-                className="text-[40px] font-bold text-white"
+                className="text-[40px] font-bold text-white capitalize"
                 dangerouslySetInnerHTML={{
-                  __html: t.markup(`${card.title}`, {
+                  __html: t.markup(card.title, {
                     highlight: (chunks: string) =>
-                      `<span class="!text-primary-brand-500">${chunks}</span>`,
+                      `<span class="text-primary-brand-500">${chunks}</span>`,
                   }),
                 }}
               />
+
               <p
                 className="font-normal text-primary-error-25 leading-[22px]"
                 dangerouslySetInnerHTML={{
@@ -40,7 +41,7 @@ function ProcessSlider() {
                 <HoverBorderGradient
                   containerClassName="rounded-full border-none"
                   as="button"
-                  className="border-white slider-gradient text-white radius-[100px] group px-5 py-3 flex items-center space-x-2"
+                  className="border-white bg-[#131F3C] text-white radius-[100px] group px-5 py-3 flex items-center space-x-2"
                 >
                   <motion.div
                     className="sparkles group-hover:block"
@@ -71,32 +72,39 @@ function ProcessSlider() {
             </div>
 
             {card.image && (
-              <Image
-                src={card.image}
-                fill
-                alt="background image"
-                className="absolute h-full w-full top-0 rounded-lg left-0 -z-10 object-cover"
-              />
+              <>
+                <div className="absolute h-full w-full top-0 left-0 rounded-lg z-[-1] bg-[#131F3C]" />
+                <Image
+                  src={card.image}
+                  width={480}
+                  height={650}
+                  alt="background image"
+                  className="absolute h-full top-[-100px] bottom-[10] right-0 rounded-lg z-[-1] object-cover"
+                />
+              </>
             )}
             {card.backgroundVideo && (
-              <motion.video
-                autoPlay
-                loop
-                muted
-                style={{
-                  position: "absolute",
-                  borderRadius: "8px",
-                  top: 0,
-                  left: 0,
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                  zIndex: -1,
-                  opacity: 1,
-                }}
-              >
-                <source src={card.backgroundVideo} type="video/mp4" />
-              </motion.video>
+              <>
+                <div className="absolute h-full w-full top-0 left-0 rounded-lg z-[-1] slider-gradient" />
+                <motion.video
+                  autoPlay
+                  loop
+                  muted
+                  style={{
+                    position: "absolute",
+                    borderRadius: "8px",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    zIndex: -2,
+                    opacity: 1,
+                  }}
+                >
+                  <source src={card.backgroundVideo} type="video/mp4" />
+                </motion.video>
+              </>
             )}
           </div>
         </SwiperSlide>
